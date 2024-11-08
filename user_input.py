7aac303093ad67a4e7ba44c2dbd652c2af0298e7
Orevaoghene Ekwa <73761990+Orevaoghene-Ekwa@users.noreply.github.com>
2024-11-08 19:30:28 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# This is the first assignment in the python module of power learn project
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("What is your location?: ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
